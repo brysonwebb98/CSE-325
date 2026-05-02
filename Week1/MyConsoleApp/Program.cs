6a0c17ab90bd3525e882ec6b4d80f11537082bbd
brysonwebb98 <brysonwebb98@gmail.com>
2026-05-01 20:58:50 -0600

    Pushing W2 Assignments

---
+++ Week1/MyConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine($"The current time is {DateTime.Now}");
+
+DateTime today = DateTime.Now;
+DateTime christmas = new DateTime(DateTime.Now.Year, 12, 25);
+int daysLeft = (christmas - today).Days;
+
+Console.WriteLine(today);
+Console.WriteLine(christmas);
+
+Console.WriteLine($"Only {daysLeft} left until Christmas!");
